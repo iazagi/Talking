cb5efb3507213b5386a3b8eec6a554136999b6cb
iazagi <101048623+iazagi@users.noreply.github.com>
2022-12-11 11:54:43 +0200

    frontend

---
+++ talking_backend/schemas/User.js
@@ -0,0 +1,18 @@
+export default {
+    name: 'user',
+    title: 'user',
+    type: 'document',
+    fields: [
+    {
+        name: 'UserName',
+        title: 'UserName',
+        type: 'string'
+    },
+    {
+        name: 'image',
+        title: 'Image',
+        type: 'string'
+    },
+]
+
+}
